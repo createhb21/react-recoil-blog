8377667fcd20864ca0b88c391e2ca824b387d505
Createhb21 <alsghk9701@gmail.com>
2021-07-18 21:23:15 +0900

    first atom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReadOnlyCount from './components/example/ReadOnlyCount';
+import ReadWriteCount from './components/example/ReadWriteCount';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        atom 예시
+        <>
+          <ReadWriteCount />
+          <ReadOnlyCount />
+        </>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil'; // recoil 추가
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const app = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
+  app
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
