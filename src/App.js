5a8242e96085d4ea6ef8592e48af5fcbeaa06329
Createhb21 <alsghk9701@gmail.com>
2021-07-18 22:38:10 +0900

    useRecoilStateLoadable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReadOnlyCount from './components/example/ReadOnlyCount';
-import ReadWriteCount from './components/example/ReadWriteCount';
+// import ReadOnlyCount from './components/example/ReadOnlyCount';
+// import ReadWriteCount from './components/example/ReadWriteCount';
 import './App.css';
-import SelectorCount from './components/example/SelectorCount';
+// import SelectorCount from './components/example/SelectorCount';
+// import RecoilStarCount from './components/example/RecoilStarCount';
+import DelayCount from './components/example/DelayCount';
+
 
 function App() {
   return (
     <div className="App">
         atom 예시
         <>
-          <ReadWriteCount />
+          {/* <ReadWriteCount />
           <ReadOnlyCount />
           <SelectorCount />
+          <RecoilStarCount /> */}
+          <DelayCount />
         </>
     </div>
   );
---
+++ src/recoil/recoilStar.js
@@ -0,0 +1,13 @@
+// recoil/recoilStar.js
+import { selector } from 'recoil';
+
+// 비동기 처리 셀렉터
+export const recoilStarCountState = selector({
+    key: 'asyncState',
+    get: async () => {
+        const response = await fetch('https://api.github.com/repos/facebookexperimental/Recoil');
+        const recoilProjectInfo = await response.json();
+        // stargazers_count 반환
+        return recoilProjectInfo['stargazers_count'];
+    },
+});
